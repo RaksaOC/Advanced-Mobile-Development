254e7dc51284a283d401894fa9e50d6e2db1406e
Chanraksa Ory <OCraksa@gmail.com>
2026-03-01 21:54:39 +0700

    FINISH W6

---
+++ w6/lib/ui/states/settings_state.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/widgets.dart';
+
+import '../../data/repositories/settings/app_settings_repository.dart';
+import '../../model/settings/app_settings.dart';
+
+class AppSettingsState extends ChangeNotifier {
+  final AppSettingsRepository _repository;
+  AppSettings? _appSettings;
+
+  AppSettingsState(this._repository);
+
+  Future<void> init() async {
+    _appSettings = await _repository.load();
+    notifyListeners();
+  }
+
+  ThemeColor get theme => _appSettings?.themeColor ?? ThemeColor.blue;
+
+  Future<void> changeTheme(ThemeColor themeColor) async {
+    if (_appSettings == null) return;
+    _appSettings = _appSettings!.copyWith(themeColor: themeColor);
+    await _repository.save(_appSettings!);
+    notifyListeners();
+  }
+}
